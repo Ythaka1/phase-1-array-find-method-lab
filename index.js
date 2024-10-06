f9df83b0d011b9c0b81db40f1b3786f122d16922
Hakim Waithaka <hakim.waithaka@gmail.com>
2024-10-06 22:32:53 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record) {
+    
+    const winRecord = record.find(game => game.team === "Denver Broncos" && game.result === "W");
+    
+   
+    return winRecord ? winRecord.year : undefined;
+}
+
+return winRecord ? winRecord.year : undefined;
+
+  
